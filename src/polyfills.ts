a4a6e34fea5431e40af54baa56997093a666c2eb
polbeik <61149462+polbeik@users.noreply.github.com>
2020-06-08 19:48:10 +0300

    Update polyfills.ts

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -63,8 +63,3 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  */
 
 
-// (window as any).process = {
-//     env: { DEBUG: undefined }
-//   };
-  
-//   (window as any).global = window;
